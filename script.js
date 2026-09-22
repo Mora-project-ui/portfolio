60d55a6a91341e2fae9de3151a185ce978af843f
MOHAMMAD RAMA AL-JANNAH <mramaaljannah@gmail.com>
2026-09-22 21:23:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const cards = document.querySelectorAll(".project-card");
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+cards.forEach(card => observer.observe(card));
